544dd24ccf20e6c2bb9a511bffde586eda8326e5
tsaichiehhuang <a0970605512@gmail.com>
2024-02-29 17:30:16 +0800

    fix(color): set default colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,58 @@
-import type { Config } from 'tailwindcss'
+/** @type {import('tailwindcss').Config} */
+const { nextui } = require('@nextui-org/react')
 
-const config: Config = {
-    content: [
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
+module.exports = {
+    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-            },
             fontFamily: { 'Noto Sans TC': ['Noto Sans TC'], Pattaya: ['Pattaya'] },
+
+            colors: {
+                mainBlue: '#6689CB',
+                mainGreen: '#5FCC7D ',
+                mainOrange: '#F2A660 ',
+                darkGray: '#B0B0B0',
+            },
+            minHeight: {
+                56: '14rem',
+            },
         },
     },
-    plugins: [],
+    darkMode: 'class',
+    plugins: [
+        nextui({
+            prefix: 'nextui', // prefix for themes variables
+            addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+            defaultTheme: 'light', // default theme from the themes object
+            defaultExtendTheme: 'light', // default theme to extend on custom themes
+            layout: {}, // common layout tokens (applied to all themes)
+            themes: {
+                light: {
+                    layout: {}, // light theme layout tokens
+                    colors: {
+                        background: '#FDFDFD',
+                        foreground: '#11181C',
+                        primary: {
+                            foreground: '#FFFFFF',
+                            DEFAULT: '#6689CB',
+                        },
+                        success: '#7FE69C',
+                        bordered: '#333',
+                        warning: { DEFAULT: '#F2A660', foreground: '#000000' },
+                    },
+                },
+                dark: {
+                    layout: {},
+                    colors: {
+                        background: '#171E28',
+                        foreground: '#ECEDEE',
+                        primary: {
+                            foreground: '#FFFFFF',
+                            DEFAULT: '#6689CB',
+                        },
+                    },
+                },
+            },
+        }),
+    ],
 }
-export default config
