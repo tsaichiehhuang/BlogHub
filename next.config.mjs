d00127a70cbb8b63d42e4ad8a91bfb354ae5145e
tsaichiehhuang <a0970605512@gmail.com>
2024-03-25 04:07:01 +0800

    vercel env variable can't run

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        NEXT_PUBLIC_CLIENT_ID: process.env.NEXT_PUBLIC_CLIENT_ID,
+        NEXT_PUBLIC_CLIENT_SECRET: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+        NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+        NEXT_PUBLIC_GITHUB_OWER_NAME: process.env.NEXT_PUBLIC_GITHUB_OWER_NAME,
+        NEXT_PUBLIC_GITHUB_REPO_NAME: process.env.NEXT_PUBLIC_GITHUB_REPO_NAME,
+    },
     reactStrictMode: false,
     headers: async () => {
         return [
