564df3ad93df2b5a130ab7ea52e0a2fa6a4c284e
tsaichiehhuang <a0970605512@gmail.com>
2024-03-25 20:59:04 +0800

    fix: env variable

--- environment.d.ts
+++ environment.d.ts
@@ -1,9 +1,7 @@
 namespace NodeJS {
     interface ProcessEnv {
         NEXT_PUBLIC_GITHUB_CLIENT_ID: string
-        NEXT_PUBLIC_SITE_URL: string
         NEXT_PUBLIC_GITHUB_CLIENT_SECRET: string
-        NEXT_PUBLIC_GITHUB_OWNER_NAME: string
-        NEXT_PUBLIC_GITHUB_REPO_NAME: string
+        NEXT_PUBLIC_SITE_URL: string
     }
 }
