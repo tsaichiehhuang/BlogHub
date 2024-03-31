8a8fb4625e70bd089dbee690fce070c746a5c005
tsaichiehhuang <a0970605512@gmail.com>
2024-03-31 19:41:00 +0800

    fix(header): show username after login

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,11 +2,13 @@ import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/reac
 import IsLogin from '@/components/IsLogin'
 import Logo from './Logo'
 import dynamic from 'next/dynamic'
+import { cookies } from 'next/headers'
 const LoginButton = dynamic(() => import('@/components/header/LoginButton'))
 const LogoutButton = dynamic(() => import('@/components/header/LogoutButton'))
 
 export default function Header() {
     const { isAuthorLogin, isUserLogin } = IsLogin()
+    const username = cookies().get('username')
 
     return (
         <Navbar maxWidth="full">
@@ -18,7 +20,7 @@ export default function Header() {
                 <NavbarItem className="text-right">
                     {isAuthorLogin || isUserLogin ? (
                         <div className="flex flex-row items-center">
-                            <div className="font-bold text-md">嗨,丹尼爾</div> <LogoutButton />
+                            <div className="font-bold text-md">嗨,{username?.value}</div> <LogoutButton />
                         </div>
                     ) : (
                         <LoginButton />
